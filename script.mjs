99ac31dd02f288e155ff43cc8da39573ce15c9bd
Kaalhode <kennyjdang123@gmail.com>
2025-01-16 15:17:34 +0100

    La til dikt i poem

--- script.mjs
+++ script.mjs
@@ -4,7 +4,8 @@ import HTTP_CODES from './utils/httpCodes.mjs';
 const server = express();
 const port = (process.env.PORT || 8000);
 
-const poem = "poem";
+const poem = "Ensom Alene i stillhet, går vi hver vår vei Ensomheten fyller oss Glede er noe vi ikke kjenner til. Men vi vet noe i livet. Sannheten. Snart vil folk forstå. Enn så lenge, gjør vi som vi gjør. Vi vet da, hva som skjer i morgen. I morgen møter vi verden med vår ensomhet. Dikt skrevet av Snowder (ris og ros) tatt fra dikt.org";
+
 server.set('port', port);
 server.use(express.static('public'));
 
