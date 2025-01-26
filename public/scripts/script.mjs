b88c12858c4b7d6a22b24585f7fa81323891b80b
Kaalhode <kennyjdang123@gmail.com>
2025-01-26 14:50:18 +0100

    la til gettere for å hente knappene og select

---
+++ public/scripts/script.mjs
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const createDeckButton = document.getElementById('createDeck');
+    const shuffleDeckButton = document.getElementById('shuffleDeck');
+    const drawCardButton = document.getElementById('drawCard');
+    const getDeckButton = document.getElementById('getDeck');
+    const deckSelector = document.getElementById('deckSelector');
+    const cardDisplay = document.getElementById('cardDisplay');
+    let deckId = null;
+    let decks = {};
+
+});
