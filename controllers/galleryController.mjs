8fbf892a3c15d144b278633bb78f7c4f659ea7cf
Kaalhode <kennyjdang123@gmail.com>
2025-02-23 00:24:51 +0100

    Glemte å legge til eksport på gallerycontroller, og lagde ferdig router for gallery

--- controllers/galleryController.mjs
+++ controllers/galleryController.mjs
@@ -2,13 +2,13 @@ import ImageGallery from '../models/imageGallery.mjs';
 import HTTP_CODES from './utils/httpCodes.mjs';
 const gallery = new ImageGallery();
 
-function addImage(req, res) {
+export function addImage(req, res) {
     const { imageId, imageUrl } = req.body;
     gallery.addImage(imageId, imageUrl);
     res.status(HTTP_CODES.SUCCESS.OK).send('Image added successfully');
 };
 
-function findImageById(req, res) {
+export function findImageById(req, res) {
     const imageId = req.params.imageId;
     const image = gallery.findImageById(imageId);
     if(image){
@@ -18,19 +18,19 @@ function findImageById(req, res) {
     }
 };
 
-function deleteImageById(req, res){
+export function deleteImageById(req, res){
     const imageId = req.params.imageId;
     gallery.deleteImageById(imageId);
     res.status(HTTP_CODES.SUCCESS.OK).send('Image deleted sucessfully');
 };
 
-function updateImageUrl(req,res){
+export function updateImageUrl(req,res){
     const { imageId, newImageUrl } = req.body;
     gallery.updateImageUrl(imageId, newImageUrl);
     res.status(HTTP_CODES.SUCCESS.OK).send('Image Url updated sucessfully');
 };
 
-function displayGallery(req,res){
+export function displayGallery(req,res){
     const images = gallery.displayGallery();
     res.status(HTTP_CODES.SUCCESS.OK).json(images);
 }
--- routes/gallery.mjs
+++ routes/gallery.mjs
@@ -1,2 +1,11 @@
 import express from 'express'
+import {addImage,findImageById,deleteImageById, updateImageUrl, displayGallery} from '../controllers/galleryController.mjs'
+const router = express.Router();
 
+router.post('/add', addImage)
+router.get('/find/:imageId', findImageById);
+router.delete('delete/:imageId', deleteImageById);
+router.put('/update', updateImageUrl);
+router.get('/', displayGallery)
+
+export default router;
