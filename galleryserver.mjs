24caf34febe12d2930d0053f4c5ab88919d1fede
Kaalhode <kennyjdang123@gmail.com>
2025-02-23 00:32:22 +0100

    la til router og fjernet statuskodene, siden det ligger i controller filen (funksjonene). kanskje det er nødvendig senere.

--- galleryserver.mjs
+++ galleryserver.mjs
@@ -1,2 +1,17 @@
 import express from 'express'
-import HTTP_CODES from './utils/httpCodes.mjs';
+import galleryRouter from './routes/gallery.mjs';
+
+const server = express();
+const port = 8001;
+
+server.use(express.json());
+
+server.use('/gallery', galleryRouter);
+
+server.get('/', (req, res) => {
+    res.redirect('/gallery');
+});
+
+server.listen(port, () => {
+    console.log(`Gallery app listening at http://localhost:${port}`);
+});
