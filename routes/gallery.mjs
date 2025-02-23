604e153a8431cd950cd84444ed6a067bbf3af9b6
Kaalhode <kennyjdang123@gmail.com>
2025-02-23 01:25:03 +0100

    glemte en / foran delete...

--- routes/gallery.mjs
+++ routes/gallery.mjs
@@ -4,7 +4,7 @@ const router = express.Router();
 
 router.post('/add', addImage)
 router.get('/find/:imageId', findImageById);
-router.delete('delete/:imageId', deleteImageById);
+router.delete('/delete/:imageId', deleteImageById);
 router.put('/update', updateImageUrl);
 router.get('/', displayGallery)
 
